cc4d39204893c608054299538af8a6c0410c008b
Shubh959 <101618252+Shubh959@users.noreply.github.com>
2022-03-15 12:08:05 +0530

    java file uploded

---
+++ Shubham.java
@@ -0,0 +1,8 @@
+class Shub{
+public static void main(String ar[])
+{
+System.out.println("hello student");//hello
+System.out.print("Shubham/n");
+System.out.println("MIT College/n");
+System.out.println("Mechanical/n");
+}}
